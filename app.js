6bab081ef4611f7cc5edacedd21c7f2b4c82fdad
Rafael Teixeira <rafaelfteixeira@hotmail.com>
2018-09-12 23:03:11 -0300

    Compilacao de sass

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 var treino = require("./src/treino");
+require('./node_modules/digix-ui/ativos/bibliotecas/fontawesome/fontawesome')
+require('./node_modules/digix-ui/ativos/bibliotecas/fontawesome/light.min')
 
 treino.exibirAlerta();
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,9 +2,33 @@ const path = require('path');
 
 module.exports = {
   mode: 'none',
-  entry: './app.js',
+  entry: ['./app.js', './app.scss'],
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].css',
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   }
 }
